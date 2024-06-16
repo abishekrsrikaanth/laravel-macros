2b8616902d9b271aaa4e64e42815460253635fcc
Abishek R Srikaanth <abishek@technicaliti.me>
2024-06-16 17:22:12 +0530

    Loading all Stringable macros and adding idehelper stubs

---
+++ ide-helper.stubs.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Illuminate\Support {
+    class Str
+    {
+        public function abbreviate($subject, $delimiter = ' ', $glue = ''): string
+        {
+            return '';
+        }
+    }
+}
+
+namespace Illuminate\Support {
+
+    class Stringable
+    {
+        public function abbreviate($subject, $delimiter = ' ', $glue = ''): string
+        {
+            return '';
+        }
+    }
+}
--- src/MacrosServiceProvider.php
+++ src/MacrosServiceProvider.php
@@ -4,8 +4,10 @@ namespace WDR\Macros;
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use WDR\Macros\Macros\Stringable\Abbreviate;
 
 class MacrosServiceProvider extends PackageServiceProvider
 {
@@ -21,19 +23,23 @@ class MacrosServiceProvider extends PackageServiceProvider
         Collection::make($this->getStringMacros())
             ->reject(fn ($class, $macro) => Str::hasMacro($macro))
             ->each(fn ($class, $macro) => Str::macro($macro, app($class)()));
+
+        Collection::make($this->getStringableMacros())
+            ->reject(fn ($class, $macro) => Stringable::hasMacro($macro))
+            ->each(fn ($class, $macro) => Stringable::macro($macro, app($class)()));
     }
 
     public function getStringMacros(): array
     {
         return [
-            'abbreviate' => \WDR\Macros\Macros\String\Abbreviate::class,
+            'abbreviate' => Macros\String\Abbreviate::class,
         ];
     }
 
     public function getStringableMacros(): array
     {
         return [
-            'abbreviate' => \WDR\Macros\Macros\Stringable\Abbreviate::class,
+            'abbreviate' => Abbreviate::class,
         ];
     }
 }
